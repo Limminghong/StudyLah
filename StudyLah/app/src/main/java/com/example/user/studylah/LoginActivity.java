7427453afc3d7176fd3096914575d41b4525e1ce
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-05-27 19:38:10 +0800

    Added user authentication, Toast bugs yet to be fixed

---
+++ StudyLah/app/src/main/java/com/example/user/studylah/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.user.studylah;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView mTextViewSignup;
+    private Button mButtonLogin;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPw;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Firebase Auth Instance
+        auth = FirebaseAuth.getInstance();
+
+        // Inistialise widgets
+        mButtonLogin = findViewById(R.id.buttonLogin);
+        mEditTextEmail = findViewById(R.id.editTextUsernameLogin);
+        mEditTextPw = findViewById(R.id.editTextPasswordLogin);
+        mTextViewSignup = findViewById(R.id.textViewSignup);
+
+        mTextViewSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEditTextEmail.getText().toString().trim();
+                String password = mEditTextPw.getText().toString().trim();
+                // Check if email is empty
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(LoginActivity.this, "Enter Email Address", Toast.LENGTH_SHORT).show();
+                }
+
+                // Check if password is empty
+                if(TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                }
+
+                // Set your own additional constraints
+
+                // Authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful()) {
+                                    // Error occurred
+                                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
--- StudyLah/app/src/main/java/com/example/user/studylah/MainActivity.java
+++ StudyLah/app/src/main/java/com/example/user/studylah/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.studylah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Check if user is logged in
+        // If user is not logged in, direct user to login page
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.logout:
+                userLogout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Signup User and return user to LoginActivity
+    private void userLogout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
