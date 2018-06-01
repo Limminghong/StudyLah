f6407dc264ede6cc201d92d2681d9f6ead30fb75
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-06-01 12:20:23 +0800

    Create session into firebase

--- StudyLah/app/src/main/java/com/example/user/studylah/createSession.java
+++ StudyLah/app/src/main/java/com/example/user/studylah/createSession.java
@@ -1,13 +1,71 @@
 package com.example.user.studylah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class createSession extends AppCompatActivity {
+    private EditText mEditTextModule;
+    private EditText mEditTextTiming;
+    private EditText mEditTextLocation;
+
+    private Button mButtonCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_session);
+
+        mEditTextModule = (EditText)findViewById(R.id.editTextModule);
+        mEditTextTiming = (EditText)findViewById(R.id.editTextTiming);
+        mEditTextLocation = (EditText)findViewById(R.id.editTextLocation);
+        mButtonCreate = (Button)findViewById(R.id.buttonCreate);
+
+        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        */
+
+        // Create button
+        mButtonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewSession();
+                backToMainActivity();
+            }
+        });
+    }
+
+    /*@Override
+    public boolean onSupportNavigateUp() {
+        backToHost();
+        return true;
+    }*/
+
+    private void addNewSession() {
+        String module = mEditTextModule.getText().toString().trim();
+        String timing = mEditTextTiming.getText().toString().trim();
+        String location = mEditTextLocation.getText().toString().trim();
+        // Write message to database
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("sessions");
+        // Session object to store info
+        Session session = new Session();
+        session.setModule(module);
+        session.setTiming(timing);
+        session.setLocation(location);
+        String sessionId = mDatabase.push().getKey();
+        mDatabase.child(sessionId).setValue(session);
+    }
+
+    private void backToMainActivity() {
+        Intent intent = new Intent(createSession.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
