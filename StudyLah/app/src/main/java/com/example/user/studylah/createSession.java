96ac9ed1f41529b631e04cabb9a4373282b6d1e7
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-06-07 23:07:25 +0800

    Added DatePicker

--- StudyLah/app/src/main/java/com/example/user/studylah/createSession.java
+++ StudyLah/app/src/main/java/com/example/user/studylah/createSession.java
@@ -1,12 +1,16 @@
 package com.example.user.studylah;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,13 +19,19 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class createSession extends AppCompatActivity {
+    private static final String TAG = "createSession";
+
     private EditText mEditTextModule;
     private EditText mEditTextTiming;
     private EditText mEditTextDate;
     private EditText mEditTextLocation;
 
     private Button mButtonCreate;
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +71,35 @@ public class createSession extends AppCompatActivity {
                 }
             }
         });
+
+        mEditTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        createSession.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day
+                );
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mEditTextDate.setText(date);
+            }
+        };
     }
 
     /*@Override
