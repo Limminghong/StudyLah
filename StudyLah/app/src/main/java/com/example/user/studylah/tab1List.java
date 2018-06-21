3ee7489a4326da610d49d1e069797ba450a95b0b
Aaryam Srivastava <aaryamps1@gmail.com>
2018-06-21 23:53:38 +0800

    Changing the format for ListView

--- StudyLah/app/src/main/java/com/example/user/studylah/tab1List.java
+++ StudyLah/app/src/main/java/com/example/user/studylah/tab1List.java
@@ -3,17 +3,23 @@ package com.example.user.studylah;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -35,10 +41,16 @@ public class tab1List extends Fragment {
 
         session = new Session();
         listView = (ListView) rootView.findViewById(R.id.listView);
+
         database = FirebaseDatabase.getInstance();
         ref = database.getReference("sessions");
+
+        //initialise list
         list = new ArrayList<>();
-        adapter = new ArrayAdapter<>(getActivity(),R.layout.session,R.id.session_view,list);
+
+        //initialise adapter to connect firebase data to the list
+        adapter = new ArrayAdapter<>(getActivity(),R.layout.session,R.id.module,list);
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -47,8 +59,16 @@ public class tab1List extends Fragment {
                 {
                     //receives all the information for each session
                     session = ds.getValue(Session.class);
-                    list.add(session.getModule());
+                    String module_info = session.getModule().toString() + "\n" + session.getdate().toString()
+                            + "  " + session.getLocation().toString() + "  " + session.getTiming().toString()
+                            + "  " + session.getHost().toString();
+                    //Spannable string used to make the first module line twice as big
+                    SpannableString ss1 = new SpannableString(module_info);
+                    ss1.setSpan(new RelativeSizeSpan(2f), 0,5,0);
+                    //Mimicking the concatenation of the string
+                    String final_module = "" + ss1.toString();
 
+                    list.add(final_module);
                 }
                 listView.setAdapter(adapter);
             }
