3275c27d521ad02ee2f996c324e9adf00dd21fe6
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-07-26 21:19:40 +0800

    Added star rating ui (not implemented)

--- StudyLah/app/src/main/java/com/example/user/studylah/OtherProfile.java
+++ StudyLah/app/src/main/java/com/example/user/studylah/OtherProfile.java
@@ -1,9 +1,14 @@
 package com.example.user.studylah;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +28,8 @@ public class OtherProfile extends AppCompatActivity {
     private TextView mOtherEmail;
     private TextView mOtherBio;
     private Button buttonFeedback;
-    private Button buttonReport;
+    private Button buttonRate;
+    private RatingBar mOtherBar;
 
     // User database reference
     private String userId;
@@ -40,7 +46,8 @@ public class OtherProfile extends AppCompatActivity {
         mOtherEmail = (TextView)findViewById(R.id.otherEmail);
         mOtherBio = (TextView)findViewById(R.id.otherBio);
         buttonFeedback = (Button)findViewById(R.id.giveFeedback);
-        buttonReport = (Button)findViewById(R.id.reportUser);
+        buttonRate = (Button)findViewById(R.id.rateUser);
+        mOtherBar = (RatingBar) findViewById(R.id.otherRatingBar);
 
         // Get user reference
         userId = getIntent().getStringExtra("HOST_ID");
@@ -58,10 +65,12 @@ public class OtherProfile extends AppCompatActivity {
                 String email = user.getEmail();
                 String bio = user.getBio();
                 String imageThumb = user.getImageThumb();
+                int avgStars = user.getAvgStars();
 
                 mOtherName.setText(name);
                 mOtherEmail.setText(email);
                 mOtherBio.setText(bio);
+                mOtherBar.setRating(avgStars);
 
                 // Load Image
                 if(!imageThumb.equals("default")) {
@@ -72,6 +81,35 @@ public class OtherProfile extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        // Rate user
+        buttonRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = OtherProfile.this.getLayoutInflater();
+                AlertDialog.Builder alertDig = new AlertDialog.Builder(OtherProfile.this);
+                final View dialogView = inflater.inflate(R.layout.rate_user_dialog, null);
+                alertDig.setView(dialogView);
+                alertDig.setCancelable(false);
+
+                alertDig.setMessage("Rate this user");
+                alertDig.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                alertDig.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                alertDig.create().show();
             }
         });
     }
